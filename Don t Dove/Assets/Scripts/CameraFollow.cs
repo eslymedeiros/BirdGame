5c1e447ae0bd486bd2aa8254cf3d439d60e065b2
Marlon020Victor <145376158+Marlon020Victor@users.noreply.github.com>
2024-11-19 15:47:43 -0300

    DD(V.03)

--- Don t Dove/Assets/Scripts/CameraFollow.cs
+++ Don t Dove/Assets/Scripts/CameraFollow.cs
@@ -8,10 +8,11 @@ public class CameraFollow : MonoBehaviour
     
     public Transform player;
     public float MinX, MaxX;
+    public float MinY, MaxY;
     private void FixedUpdate()
     {
         transform.position = Vector2.Lerp(transform.position, player.position, 0.1f);
 
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, MinX,MaxX),transform.position.y,transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, MinX,MaxX),Mathf.Clamp(transform.position.y, MinY,MaxY),transform.position.z);
     }
 }
